0489909ac332d43740fd48a9559160a5ecded0e0
zorkow <volker.sorge@gmail.com>
2020-06-10 20:58:37 +0100

    Basic tree parsing.

--- ts/tree.ts
+++ ts/tree.ts
@@ -32,7 +32,7 @@ export class Tree {
 
   public fromJson() {
     this.json = JSON.parse(this.jsonStr);
-    console.log(this.json);
+    this.root = Node.fromJson(this.json);
   }
 
   
@@ -42,15 +42,69 @@ export class Tree {
 export class Node {
 
   public content: Map<number, string> = new Map<number, string>();
+  public kind: string = '';
   public children: Node[] = [];
   public parent: Node = null;
   
-  constructor(public title: string, public value = 0, public kind: string = '',
+  constructor(public title: string, public value = 0,
               content: {text: string, value: number}[] = []) {
     for (let {text: text, value: value} of content) {
       this.content.set(value, text);
     }
   }
 
+  static fromJson(json: any): Node {
+    let kind = json.type || 'leaf';
+    let node: Node = null;
+    switch (kind) {
+      case 'leaf':
+        node = new Leaf(json.title, json.value);
+        break;
+      case 'nary':
+        node = new Nary(json.title, json.value, json.content || []);
+        if (json.children.length) {
+          node.children = json.children.map(Node.fromJson);
+          node.children.forEach(child => child.parent = node);
+        }
+        break;
+      case 'binary':
+        node = new Binary(json.title, json.value);
+        if (json.children.length) {
+          node.children = json.children.map(Node.fromJson);
+          node.children.forEach(child => child.parent = node);
+        }
+        break
+      default:
+        throw Error('Unknown node type.');
+    }
+    return node;
+  }
+  
+}
+
+export class Binary extends Node {
+
+  public kind = 'binary'
+  
+  constructor(public title: string, public value = 0) {
+    super(title, value, [{text: 'Yes', value: 1}, {text: 'No', value: 0}]);
+  }
+
+}
+
+export class Nary extends Node {
+  public kind = 'nary'
+  
+}
+
+
+export class Leaf extends Node {
+  public kind = 'leaf';
+  readonly children: Node[] = [];
+  // TODO: Leaves could get an action.
+  
+  constructor(title: string, value: number) {
+    super(title, value);
+  }
   
 }
