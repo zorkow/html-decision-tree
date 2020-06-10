f9dc948b1889f3916e5ac061f75711880d350c1d
zorkow <volker.sorge@gmail.com>
2020-06-11 00:26:56 +0100

    Corrects next pointer in radio buttons.

--- ts/tree.ts
+++ ts/tree.ts
@@ -34,19 +34,20 @@ export class Tree {
   public fromJson() {
     this.json = JSON.parse(this.jsonStr);
     this.root = Node.fromJson(this.json);
+    let dfs = new DepthFirst(this, (x: Node) => x);
+    dfs.result.forEach(n => n.tree = this);
   }
 
-  
+
   public toHTML(node: HTMLElement = null) {
     let dfs = new DepthFirst(this, (x: Node) => x);
-    // TODO: Visit the tree.
     dfs.result.forEach((n: Node) => {
       node.appendChild(n.toHtml());
       n.hide();
     });
     this.root.show();
   }
-  
+
 }
 
 
@@ -66,24 +67,34 @@ export class DepthFirst {
 }
 
 
+let prefix = 'DT_';
+
+let addClass = function(element: HTMLElement, ...rest: string[]) {
+  rest.forEach(x => element.classList.add(prefix + x.toUpperCase()));
+};
+
+
 let counter = 0;
 let labelCounter = 0;
 
 export class Node {
 
+  public tree: Tree;
   public content: Map<number, string> = new Map<number, string>();
   public kind: string = '';
   public children: Node[] = [];
   public parent: Node = null;
   public variable: string = 'dt_variable_' + counter++;
+  public radioButtons: HTMLElement[] = [];
+  public div: HTMLElement;
 
-  private _next: HTMLElement;
-  private _previous: HTMLElement;
-  private _div: HTMLElement;
+  private nextButton: HTMLElement;
+  private previousButton: HTMLElement;
+
 
   public nextName: string = 'Next';
   public previousName: string = 'Back';
-  
+
   constructor(public title: string, public value = 0,
               content: {text: string, value: number}[] = []) {
     for (let {text: text, value: value} of content) {
@@ -118,16 +129,21 @@ export class Node {
     return node;
   }
 
-  public radios: HTMLElement[] = [];
-  
   public toHtml(): HTMLElement {
-    let div = document.createElement('div');
-    addClass(div, 'NODE', this.kind);
+    this.div = document.createElement('div');
+    addClass(this.div, 'NODE', this.kind);
     let title = document.createElement('span');
     addClass(title, 'TITLE');
     title.innerHTML = this.title;
-    div.appendChild(title);
-    this.content.forEach(x => {
+    this.div.appendChild(title);
+    this.radios();
+    this.buttons();
+    return this.div;
+  }
+
+
+  protected radios() {
+    for (const [key, value] of this.content) {
       let content = document.createElement('div');
       addClass(content, 'CONTENT');
       let radio = document.createElement('input');
@@ -135,36 +151,43 @@ export class Node {
       let labelId = 'dt_id_' + labelCounter++;
       radio.type = 'radio';
       radio.name = this.variable;
-      radio.value = this.value.toString();
+      radio.value = key.toString();
       radio.setAttribute('aria-labelledby', labelId);
-      this.radios.push(radio);
+      this.radioButtons.push(radio);
       let label = document.createElement('label');
       addClass(label, 'LABEL');
       label.id = labelId;
-      label.innerHTML = x;
+      label.innerHTML = value;
       content.appendChild(radio);
       content.appendChild(label);
-      div.appendChild(content);
-    });
-    this._div = div;
-    this.previous();
+      this.div.appendChild(content);
+    }
+  }
+
+  protected buttons() {
+    let buttons = document.createElement('div');
+    addClass(buttons, 'BUTTONS');
+    this.div.appendChild(buttons);
+    if (this.parent) {
+      this.previous();
+      buttons.appendChild(this.previousButton);
+    }
     this.next();
-    return div;
+    buttons.appendChild(this.nextButton);
   }
 
-  public next() {
-    if (this._next) return;
-    this._next = document.createElement('button');
-    this._next.innerHTML = this.nextName;
-    addClass(this._next, 'NEXT');
-    this._next.addEventListener('click', this.fireNext.bind(this));
-    this._div.appendChild(this._next);
+  protected next() {
+    if (this.nextButton) {
+      return;
+    }
+    this.nextButton = document.createElement('button');
+    this.nextButton.innerHTML = this.nextName;
+    addClass(this.nextButton, 'NEXT');
+    this.nextButton.addEventListener('click', this.fireNext.bind(this));
   }
 
-  public fireNext() {
-    console.log('Firing');
-    for (let radio of this.radios) {
-      console.log(radio);
+  protected fireNext() {
+    for (let radio of this.radioButtons) {
       if ((radio as any).checked) {
         let value = (radio as any).value;
         console.log(value);
@@ -174,41 +197,38 @@ export class Node {
       }
     }
   }
-  
-  public previous() {
-    if (this._previous || !this.parent) return;
-    this._previous = document.createElement('button');
-    this._previous.innerHTML = this.previousName;
-    addClass(this._previous, 'PREVIOUS');
-    this._div.appendChild(this._previous);
+
+  protected previous() {
+    if (this.previousButton) return;
+    this.previousButton = document.createElement('button');
+    this.previousButton.innerHTML = this.previousName;
+    addClass(this.previousButton, 'PREVIOUS');
+    this.previousButton.addEventListener('click', this.firePrevious.bind(this));
+    this.div.appendChild(this.previousButton);
   }
+
+  protected firePrevious() {
+    if (this.parent) {
+      this.hide();
+      this.parent.show();
+    }
+  }
+
 
   public show() {
-    this._div.style.visibility = 'visible';
+    this.div.style.visibility = 'visible';
   }
-  
+
   public hide() {
-    this._div.style.visibility = 'hidden';
+    this.div.style.visibility = 'hidden';
   }
-  
+
 }
-
-let prefix = 'DT_';
-
-let addClass = function(element: HTMLElement, ...rest: string[]) {
-  rest.forEach(x => element.classList.add(prefix + x.toUpperCase()));
-};
-
-// let makeElement = function(kind: string, ...rest: string[]) {
-  
-// }
-
-
 
 export class Binary extends Node {
 
   public kind = 'binary'
-  
+
   constructor(public title: string, public value = 0) {
     super(title, value, [{text: 'Yes', value: 1}, {text: 'No', value: 0}]);
   }
@@ -218,7 +238,7 @@ export class Binary extends Node {
 
 export class Nary extends Node {
   public kind = 'nary'
-  
+
 }
 
 
@@ -227,9 +247,14 @@ export class Leaf extends Node {
   readonly children: Node[] = [];
   public nextName = 'Restart';
   // TODO: Leaves could get an action.
-  
+
   constructor(title: string, value: number) {
     super(title, value);
   }
-  
+
+  protected fireNext() {
+    this.hide();
+    this.tree.root.show();
+  }
+
 }
