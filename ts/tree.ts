61f2de1c72b895fe328c0c1f4faf4c95e6750f5a
zorkow <volker.sorge@gmail.com>
2020-06-10 19:54:10 +0100

    Initial Node class.

--- ts/tree.ts
+++ ts/tree.ts
@@ -3,6 +3,8 @@
 export class Tree {
 
   public jsonStr: string = '';
+  public json: any = null;
+  public root: Node = null;
 
   public loadJson() {
     let location = window.location.href.replace(/\/.*\.html$/, 'samples/tree2.json');
@@ -21,8 +23,32 @@ export class Tree {
   public _loadJson(json: string) {
     try {
       this.jsonStr = json;
+      this.fromJson();
     } catch (err) {
-      console.error('Something went wrong!')
+      console.error('Something went wrong!');
+      console.error(err);
+    }
+  }
+
+  public fromJson() {
+    this.json = JSON.parse(this.jsonStr);
+    console.log(this.json);
+  }
+
+  
+}
+
+
+export class Node {
+
+  public content: Map<number, string> = new Map<number, string>();
+  public children: Node[] = [];
+  public parent: Node = null;
+  
+  constructor(public title: string, public value = 0, public kind: string = '',
+              content: {text: string, value: number}[] = []) {
+    for (let {text: text, value: value} of content) {
+      this.content.set(value, text);
     }
   }
 
